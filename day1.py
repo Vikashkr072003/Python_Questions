a2a38094ed683ea9efc28f578bca90b63c33d60f
Vikash kumar <vikashkr072003@gmail.com>
2026-03-27 21:52:21 +0000

    Solved First Qestion Age Categorization

---
+++ day1.py
@@ -0,0 +1,11 @@
+# ---------Solved Questions 1 ---------
+age = int(input("Enter your name :- "))
+
+if age < 13:
+  print("This is Child")
+elif age < 19:
+    print("This is Teeneager")
+elif age < 60:
+    print("This is Adult")
+else:
+    print("This is Senior")
